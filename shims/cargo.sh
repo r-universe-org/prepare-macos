816199edd6842a01b6f7102791711b98b0277b7b
Jeroen Ooms <jeroenooms@gmail.com>
2024-01-08 23:13:45 +0100

    handle existing cargo config file

--- shims/cargo.sh
+++ shims/cargo.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-TEMP_CARGO_CONFIG="${CARGO_HOME:-$HOME/.cargo}"
-mkdir -p "$TEMP_CARGO_CONFIG"
-echo '[build]' >> "${TEMP_CARGO_CONFIG}/config.toml"
-echo 'target = "aarch64-apple-darwin"' >> "${TEMP_CARGO_CONFIG}/config.toml"
+TEMP_CARGO_CONFIG="${CARGO_HOME:-$HOME/.cargo}/config.toml"
+if [ -f "$TEMP_CARGO_CONFIG" ] && grep -qF '[build]' "$TEMP_CARGO_CONFIG"; then
+sed -i.bak '/^target = .*/d' "$TEMP_CARGO_CONFIG"
+sed -i.bak '/\[build\]/s/.*/&\
+target = "aarch64-apple-darwin"/' "$TEMP_CARGO_CONFIG"
+cat "$TEMP_CARGO_CONFIG"
+else
+mkdir -p $(dirname "${TEMP_CARGO_CONFIG}")
+echo '[build]' >> "${TEMP_CARGO_CONFIG}"
+echo 'target = "aarch64-apple-darwin"' >> "${TEMP_CARGO_CONFIG}"
+fi
+
+# Call real cargo
 "$HOME/.cargo/bin/cargo" "$@"
 
 # I could not get build.target-dir to do the right thing
